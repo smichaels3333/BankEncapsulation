04099d650c27562112977006b05dd04e1bb05f6b
Stephen Michaels <stephen.michaels3333@gmail.com>
2023-01-15 12:17:44 -0500

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"Thank you! Your balance is now {account.GetBalance}!");
         }
     }
 }
